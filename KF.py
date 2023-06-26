bf5f47d853d47b7256dfc3340143389fa4a2575d
EvvaLyj <yinjliu@student.ethz.ch>
2023-06-26 11:39:28 +0200

    Reorganize

--- KF.py
+++ KF.py
@@ -77,3 +77,75 @@ def KF(Length,dt,x0,P0,F,H,Q,R,Positions):
 #     print(filter_result)
     filter_result=np.squeeze(np.array(filter_result))
     return filter_result
+
+
+def KF_ML_ite(Length,dt,x0,P0,F,H,Q,R,lmb,ml_resol,std,arr,Phases,search_factor):
+    #dynamics: dt,x0,P0,F,H,Q,R,Length (length of a trajectory)
+    #ML parameters: lmb,ml_resol,std,arr,Phases
+    
+    # For each time step of the trajectory, run multiple iterations of Encoder + KF
+    ite_num = 3
+
+    #filter
+    kf =KalmanFilter(dim_x=4, dim_z=2) 
+    #Initialize parameters
+    kf.x = x0 #
+    kf.P = P0
+    kf.F = F
+    kf.H = H
+    kf.Q = Q
+    kf.R = R
+    
+
+    #KF filter
+    filter_result=list()
+    filter_result_all=list()
+    filter_result.append(x0)
+    filter_result_all.append(x0)
+    z_k_list = [] #start at z_k1
+    
+    for i in range(1,Length):
+        for j in range(ite_num):
+            if(j==0):
+                #predict
+                kf.predict()#get x_k|x_k-1
+                
+                # ML + grid search
+                xrange=np.array([kf.x_prior[0,0]-search_factor*std,kf.x_prior[0,0]+search_factor*std])
+                yrange=np.array([kf.x_prior[2,0]-search_factor*std,kf.x_prior[2,0]+search_factor*std])
+            
+                y_k=Phases[i,:]
+                ml=ML()
+                num_grid,Loc_MLgrid=ml.ML_grid(xrange,yrange,y_k,arr,lmb,resolution=ml_resol,flag_prior=False,flag_plot=False)
+                z = Loc_MLgrid.reshape(2,1)
+                print(f'ite{j},z={z}')
+                #update
+                kf.update(z)
+                post_x=kf.x
+
+            else:
+                # #predict
+                # kf.predict()#get x_k|x_k-1
+                
+                # ML + grid search
+                xrange=np.array([post_x[0,0]-search_factor*std,post_x[0,0]+search_factor*std])
+                yrange=np.array([post_x[2,0]-search_factor*std,post_x[2,0]+search_factor*std])
+            
+                y_k=Phases[i,:]
+                ml=ML()
+                num_grid,Loc_MLgrid=ml.ML_grid(xrange,yrange,y_k,arr,lmb,resolution=ml_resol,flag_prior=False,flag_plot=False)
+                z = Loc_MLgrid.reshape(2,1)
+                print(f'ite{j},z={z}')
+                #update
+                kf.update(z)
+                post_x=kf.x
+            
+            #store
+            filter_result_all.append(kf.x)
+            if(j==ite_num-1):
+                z_k_list.append(Loc_MLgrid.reshape(2,1)) #z_k
+                filter_result.append(kf.x)
+#     print(filter_result)
+    filter_result=np.squeeze(np.array(filter_result))
+    z_k_list = np.array(z_k_list)
+    return z_k_list,filter_result,filter_result_all
